532924eeee5b413fab3c8b5ea1469724e5b76b30
Diego Toledo <dbtoledoa@gmail.com>
2022-08-20 19:02:26 -0500

    feat: add feature new people to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,18 +5,36 @@ import Person from "./components/Person";
 function App() {
   const people = [
     {
-      id: 1,
       name: "Arto Hellas",
     },
   ];
   const [persons, setPersons] = useState(people);
   const [newName, setNewName] = useState("");
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  const inputPersonHandler = (event) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div className="App">
       <h2>PhoneBook</h2>
-      <form>
+      <form onSubmit={addNewPerson}>
         <div>
-          name: <input />
+          name:{" "}
+          <input
+            placeholder="Add new name here..."
+            value={newName}
+            onChange={inputPersonHandler}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -25,7 +43,7 @@ function App() {
       <h2>Numbers: </h2>
       {persons.map((el) => {
         return (
-          <ul key={el.id}>
+          <ul key={el.name}>
             <Person person={el} />
           </ul>
         );
